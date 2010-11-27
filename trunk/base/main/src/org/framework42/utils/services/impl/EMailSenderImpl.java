080a9d0c43c6176b4895d76d0676fa3080fcbaf6
drbizzaro <drbizzaro@97581d6c-679a-47f2-af3c-956745aba767>
2010-11-27 19:31:00 +0000

    Refactoring, unit tests and documentation of base library.

--- trunk/base/main/src/org/framework42/utils/services/impl/EMailSenderImpl.java
+++ trunk/base/main/src/org/framework42/utils/services/impl/EMailSenderImpl.java
@@ -1,7 +1,7 @@
 package org.framework42.utils.services.impl;
 
-import org.framework42.utils.services.EMailSender;
 import org.apache.log4j.Logger;
+import org.framework42.utils.services.EMailSender;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
@@ -10,49 +10,49 @@ import java.util.Properties;
 
 public enum EMailSenderImpl implements EMailSender {
 
-   INSTANCE;
+    INSTANCE;
 
-   private final Logger logger = Logger.getLogger("com.nummer42.poeter");
+    private final Logger logger = Logger.getLogger("org.framework42");
 
-   private final String mailServerURL;
+    private final String mailServerURL;
 
-   private EMailSenderImpl() {
+    private EMailSenderImpl() {
 
-      mailServerURL = "localhost";
+        mailServerURL = "localhost";
 
-   }
+    }
 
-   public void sendSimpleEMail(String subject, String messageText, String fromAddress, String toAddress){
+    public void sendSimpleEMail(String subject, String messageText, String fromAddress, String toAddress) {
 
-      Properties MAIL_SETTINGS = new Properties();
-      MAIL_SETTINGS.setProperty("mail.transport.protocol", "smtp");
-      MAIL_SETTINGS.setProperty("mail.host", mailServerURL);
+        Properties MAIL_SETTINGS = new Properties();
+        MAIL_SETTINGS.setProperty("mail.transport.protocol", "smtp");
+        MAIL_SETTINGS.setProperty("mail.host", mailServerURL);
 
-      try{
+        try {
 
-         Session mailSession = Session.getDefaultInstance(MAIL_SETTINGS, null);
-         Transport transport = mailSession.getTransport();
+            Session mailSession = Session.getDefaultInstance(MAIL_SETTINGS, null);
+            Transport transport = mailSession.getTransport();
 
-         MimeMessage message = new MimeMessage(mailSession);
-         message.setSubject(subject);
-         message.setContent(messageText, "text/plain");
-         message.setFrom(new InternetAddress(fromAddress));
-         message.addRecipient(Message.RecipientType.TO,
-                 new InternetAddress(toAddress));
+            MimeMessage message = new MimeMessage(mailSession);
+            message.setSubject(subject);
+            message.setContent(messageText, "text/plain");
+            message.setFrom(new InternetAddress(fromAddress));
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(toAddress));
 
-         transport.connect();
-         transport.sendMessage(message,
-                 message.getRecipients(Message.RecipientType.TO));
-         transport.close();
+            transport.connect();
+            transport.sendMessage(message,
+                    message.getRecipients(Message.RecipientType.TO));
+            transport.close();
 
-      }catch(NoSuchProviderException e){
-         logger.fatal("EMailSender.sendSimpleEMail: "+e);
-         throw new RuntimeException();
-      }catch(MessagingException e){
-         logger.fatal("EMailSender.sendSimpleEMail: "+e);
-         throw new RuntimeException();
-      }
+        } catch (NoSuchProviderException e) {
+            logger.fatal("EMailSender.sendSimpleEMail: " + e);
+            throw new RuntimeException();
+        } catch (MessagingException e) {
+            logger.fatal("EMailSender.sendSimpleEMail: " + e);
+            throw new RuntimeException();
+        }
 
-   }
+    }
 
 }
--- trunk/base/main/src/org/framework42/utils/services/impl/HashMakerImpl.java
+++ trunk/base/main/src/org/framework42/utils/services/impl/HashMakerImpl.java
@@ -1,7 +1,7 @@
 package org.framework42.utils.services.impl;
 
-import org.framework42.utils.services.HashMaker;
 import org.apache.log4j.Logger;
+import org.framework42.utils.services.HashMaker;
 
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
@@ -9,47 +9,47 @@ import java.security.NoSuchAlgorithmException;
 
 public enum HashMakerImpl implements HashMaker {
 
-   INSTANCE;
+    INSTANCE;
 
-   Logger logger = Logger.getLogger("com.nummer42.poeter");
+    Logger logger = Logger.getLogger("org.framework42");
 
-   private HashMakerImpl() {
-   }
+    private HashMakerImpl() {
+    }
 
-   public String getHash(String salt, String stringToHash) throws NullPointerException{
+    public String getHash(String salt, String stringToHash) throws IllegalArgumentException {
 
-      if(salt==null||stringToHash==null){
-         throw new NullPointerException();
-      }
+        if (salt == null || stringToHash == null) {
+            throw new IllegalArgumentException("Salt or stringToHash has a illegal value. Salt: " + salt + " and stringToHash: " + stringToHash);
+        }
 
-      String passHash = "";
+        String passHash = "";
 
-      try{
+        try {
 
-         byte[] theTextToDigestAsBytes = (salt+stringToHash).getBytes("UTF-8");
-         MessageDigest md = MessageDigest.getInstance("SHA-256");
-         md.update( theTextToDigestAsBytes );
-         byte[] digest = md.digest();
+            byte[] theTextToDigestAsBytes = (salt + stringToHash).getBytes("UTF-8");
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(theTextToDigestAsBytes);
+            byte[] digest = md.digest();
 
-         for ( byte b : digest ){
-            if(Integer.toHexString( b & 0xff ).length()==1){
-               passHash += "0"+Integer.toHexString( b & 0xff );
-            }else{
-               passHash += Integer.toHexString( b & 0xff );
+            for (byte b : digest) {
+                if (Integer.toHexString(b & 0xff).length() == 1) {
+                    passHash += "0" + Integer.toHexString(b & 0xff);
+                } else {
+                    passHash += Integer.toHexString(b & 0xff);
+                }
+
             }
 
-         }
+        } catch (UnsupportedEncodingException e) {
+            logger.fatal("HashMakerImpl.getHash: " + e);
+            throw new RuntimeException();
+        } catch (NoSuchAlgorithmException e) {
+            logger.fatal("HashMakerImpl.getHash: " + e);
+            throw new RuntimeException();
+        }
 
-      }catch(UnsupportedEncodingException e){
-         logger.fatal("HashMaker.getHash: "+e);
-         throw new RuntimeException();
-      }catch(NoSuchAlgorithmException e){
-         logger.fatal("HashMaker.getHash: "+e);
-         throw new RuntimeException();
-      }
+        return passHash;
 
-      return passHash;
-
-   }
+    }
 
 }
