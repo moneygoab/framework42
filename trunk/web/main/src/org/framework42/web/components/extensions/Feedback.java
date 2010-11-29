7d0dff83ed5b17961038c36b673769ee6edf5276
drbizzaro <drbizzaro@97581d6c-679a-47f2-af3c-956745aba767>
2010-11-29 20:43:48 +0000

    * Major refactoring.

--- trunk/web/main/src/org/framework42/web/components/extensions/Feedback.java
+++ trunk/web/main/src/org/framework42/web/components/extensions/Feedback.java
@@ -1,4 +1,4 @@
-package org.framework42.web.components.extentions;
+package org.framework42.web.components.extensions;
 
 import org.framework42.web.components.EventComponentBuilder;
 import org.framework42.web.components.HtmlComponent;
@@ -6,89 +6,89 @@ import org.framework42.web.utils.Util;
 
 public class Feedback extends HtmlComponent {
 
-   private Builder builder;
+    private Builder builder;
 
-   private Feedback(Builder builder) {
+    private Feedback(Builder builder) {
 
-      this.builder = builder;
+        this.builder = builder;
 
-   }
+    }
 
-   @Override
-   protected void generateHtmlSpecific(HtmlComponent parent, boolean onSameRow) {
+    @Override
+    protected void generateHtmlSpecific(HtmlComponent parent, boolean onSameRow) {
 
-      htmlBuilder.append(Util.tab(tabs));
-      htmlBuilder.append("<span id=\"");
-      htmlBuilder.append(builder.id);
-      htmlBuilder.append("\" style=\"");
-      htmlBuilder.append(builder.style);
-      htmlBuilder.append("\"");
+        htmlBuilder.append(Util.tab(tabs));
+        htmlBuilder.append("<span id=\"");
+        htmlBuilder.append(builder.id);
+        htmlBuilder.append("\" style=\"");
+        htmlBuilder.append(builder.style);
+        htmlBuilder.append("\"");
 
-      htmlBuilder.append(builder.addGeneralComponents());
+        htmlBuilder.append(builder.addGeneralComponents());
 
-      if(builder.title!=null){
-         htmlBuilder.append(" title=\"");
-         htmlBuilder.append(builder.title);
-         htmlBuilder.append("\"");
-      }
+        if (builder.title != null) {
+            htmlBuilder.append(" title=\"");
+            htmlBuilder.append(builder.title);
+            htmlBuilder.append("\"");
+        }
 
-      if(builder.className!=null){
-         htmlBuilder.append(" class=\"");
-         htmlBuilder.append(builder.className);
-         htmlBuilder.append("\"");
-      }
+        if (builder.className != null) {
+            htmlBuilder.append(" class=\"");
+            htmlBuilder.append(builder.className);
+            htmlBuilder.append("\"");
+        }
 
-      htmlBuilder.append(">\n");
+        htmlBuilder.append(">\n");
 
-      htmlBuilder.append(Util.tab(tabs+1));
-      htmlBuilder.append(builder.message);
-      htmlBuilder.append("\n");
+        htmlBuilder.append(Util.tab(tabs + 1));
+        htmlBuilder.append(builder.message);
+        htmlBuilder.append("\n");
 
-      htmlBuilder.append(Util.tab(tabs));
-      htmlBuilder.append("</span>\n");
+        htmlBuilder.append(Util.tab(tabs));
+        htmlBuilder.append("</span>\n");
 
-      html = htmlBuilder.toString();
+        html = htmlBuilder.toString();
 
-   }
+    }
 
-   public static class Builder extends EventComponentBuilder {
+    public static class Builder extends EventComponentBuilder {
 
-      private final String id;
+        private final String id;
 
-      private final String message;
+        private final String message;
 
-      private String style = "color:red;";
+        private String style = "color:red;";
 
-      private String className = null;
+        private String className = null;
 
-      private String title = null;
+        private String title = null;
 
-      public Builder(String id, String message) {
-         this.id = id;
-         this.message = message;
-      }
+        public Builder(String id, String message) {
+            this.id = id;
+            this.message = message;
+        }
 
-      public Builder style(String style){
-         this.style = style;
-         return this;
-      }
+        public Builder style(String style) {
+            this.style = style;
+            return this;
+        }
 
-      public Builder title(String title){
-         this.title = title;
-         return this;
-      }
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
 
-      public Builder className(String className){
-         this.className = className;
-         return this;
-      }
+        public Builder className(String className) {
+            this.className = className;
+            return this;
+        }
 
-      @Override
-      public Feedback build() {
-         return new Feedback(this);
-      }
-      
-   }
+        @Override
+        public Feedback build() {
+            return new Feedback(this);
+        }
 
-      
+    }
+
+
 }
