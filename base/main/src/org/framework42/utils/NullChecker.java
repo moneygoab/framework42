5e7942f433da9737d1e299fd917b44115341e117
drbizzaro <drbizzaro@97581d6c-679a-47f2-af3c-956745aba767>
2011-01-19 16:23:25 +0000

    Continued building data structure for bank system from design documents.

--- base/main/src/org/framework42/utils/NullChecker.java
+++ base/main/src/org/framework42/utils/NullChecker.java
@@ -2,20 +2,47 @@ package org.framework42.utils;
 
 import org.apache.log4j.Logger;
 
+/**
+ * This is a utility class that provides a general null checking capability that handles error handling and
+ * logging in the null handling process.
+ * */
 public class NullChecker {
 
     private final static Logger logger = Logger.getLogger("org.framework42");
 
+    /**
+     * Checks if the variable is null or not, using the default logger and a default error message.
+     * @param variable                      The variable to do a null check on.
+     * @return T                            Returns the variable sent in if it's not null.
+     * @throws IllegalArgumentException     Thrown if the tested variable is null.
+     * */
     public static <T> T notNull(T variable) {
 
         return notNull(variable, "Variable can't be null", logger);
     }
 
+    /**
+     * Checks if the variable is null or not, using the default logger and a error message that is provided
+     * by the caller.
+     * @param variable                      The variable to do a null check on.
+     * @param errorMessage                  A provided error message, that will make more sense then the default one.
+     * @return T                            Returns the variable sent in if it's not null.
+     * @throws IllegalArgumentException     Thrown if the tested variable is null.
+     * */
     public static <T> T notNull(T variable, String errorMessage) {
 
         return notNull(variable, errorMessage, logger);
     }
 
+    /**
+     * Checks if the variable is null or not, using the default logger and a error message that is provided
+     * by the caller.
+     * @param variable                      The variable to do a null check on.
+     * @param errorMessage                  A provided error message, that will make more sense then the default one.
+     * @param loggerId                      The id of the logger to use instead of the default logger.
+     * @return T                            Returns the variable sent in if it's not null.
+     * @throws IllegalArgumentException     Thrown if the tested variable is null.
+     * */
     public static <T> T notNull(T variable, String errorMessage, String loggerId) {
 
         Logger logger = Logger.getLogger(loggerId);
@@ -23,6 +50,15 @@ public class NullChecker {
         return notNull(variable, errorMessage, logger);
     }
 
+    /**
+     * Checks if the variable is null or not, using the default logger and a error message that is provided
+     * by the caller.
+     * @param variable                      The variable to do a null check on.
+     * @param errorMessage                  A provided error message, that will make more sense then the default one.
+     * @param logger                        The logger to use instead of the default logger.
+     * @return T                            Returns the variable sent in if it's not null.
+     * @throws IllegalArgumentException     Thrown if the tested variable is null.
+     * */
     public static <T> T notNull(T variable, String errorMessage, Logger logger) {
 
         if(variable == null) {
