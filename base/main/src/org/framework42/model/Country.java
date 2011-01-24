c361f6c6e8670a0895d693a5cb913d7dbcba492f
drbizzaro <drbizzaro@97581d6c-679a-47f2-af3c-956745aba767>
2011-01-24 16:50:48 +0000

    * Continued building data structure for bank system from design documents. * Building Service and DAO layer for Customer and assosiated classes.

--- base/main/src/org/framework42/model/Country.java
+++ base/main/src/org/framework42/model/Country.java
@@ -2,33 +2,63 @@ package org.framework42.model;
 
 public enum Country {
 
-    DENMARK("DK"),
-    NORWAY("NO"),
-    SWEDEN("SE"),
-    FINLAND("FI");
+    DENMARK("DK", "DNK", 208),
+    NORWAY("NO", "NOR", 578),
+    SWEDEN("SE", "SWE", 752),
+    FINLAND("FI", "FIN", 246);
 
-    private final String countryCode;
+    private final String alpha2Code;
 
-    private Country(String countryCode) {
+    private final String alpha3Code;
 
-        this.countryCode = countryCode;
+    private final int numericCode;
+
+    private Country(String alpha2Code, String alpha3Code, int numericCode) {
+
+        this.alpha2Code = alpha2Code;
+        this.alpha3Code = alpha3Code;
+        this.numericCode = numericCode;
 
     }
 
     public String getCountryCode() {
-        return countryCode;
+        return alpha2Code;
+    }
+
+    public String getAlpha2Code() {
+        return alpha2Code;
+    }
+
+    public String getAlpha3Code() {
+        return alpha3Code;
+    }
+
+    public int getNumericCode() {
+        return numericCode;
+    }
+
+    public static Country getById(int id) {
+
+        for(Country country: Country.values()) {
+
+            if(id == country.getNumericCode()) {
+                return country;
+            }
+
+        }
+
+        throw new IllegalArgumentException("No country with id "+id+" exists!");
+
     }
 
     public static Country findFromCountryCode(String countryCode) {
 
-        if (countryCode.equalsIgnoreCase("SE")) {
-            return SWEDEN;
-        } else if (countryCode.equalsIgnoreCase("DK")) {
-            return DENMARK;
-        } else if (countryCode.equalsIgnoreCase("FI")) {
-            return FINLAND;
-        } else if (countryCode.equalsIgnoreCase("NO")) {
-            return NORWAY;
+        for(Country country: Country.values()) {
+
+            if(country.getAlpha2Code().equalsIgnoreCase(countryCode)) {
+                return country;
+            }
+
         }
 
         throw new IllegalArgumentException("No country exists with country code " + countryCode);
