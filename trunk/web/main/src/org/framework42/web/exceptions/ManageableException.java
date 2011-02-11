e7ccc7bf72b5aadf80bcec0b848a5cd4a985e141
drbizzaro <drbizzaro@97581d6c-679a-47f2-af3c-956745aba767>
2011-02-11 08:21:51 +0000

    Renamed från ManagablePageException to ManagableException.

---
+++ trunk/web/main/src/org/framework42/web/exceptions/ManageableException.java
@@ -0,0 +1,11 @@
+package org.framework42.web.exceptions;
+
+public class ManageableException extends Exception {
+
+    public ManageableException() {
+    }
+
+    public ManageableException(String message) {
+        super(message);
+    }
+}
