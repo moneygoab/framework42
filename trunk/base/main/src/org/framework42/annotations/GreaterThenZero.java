d41f6436fcc9552db3206e709bada5db4e0def4f
drbizzaro <drbizzaro@97581d6c-679a-47f2-af3c-956745aba767>
2011-03-07 16:03:07 +0000

    Proxified all DAOs.

---
+++ trunk/base/main/src/org/framework42/annotations/GreaterThenZero.java
@@ -0,0 +1,11 @@
+package org.framework42.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER})
+public @interface GreaterThenZero {
+}
