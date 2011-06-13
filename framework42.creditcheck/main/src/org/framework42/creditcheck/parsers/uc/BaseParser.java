75b0fae13f2a258f1f8fd66696de3d04d3fdb9da
drbizzaro <drbizzaro@97581d6c-679a-47f2-af3c-956745aba767>
2011-06-13 11:49:28 +0000

    * Moving credit checks to own module

---
+++ framework42.creditcheck/main/src/org/framework42/creditcheck/parsers/uc/BaseParser.java
@@ -0,0 +1,47 @@
+package org.framework42.creditcheck.parsers.uc;
+
+import org.apache.log4j.Logger;
+import org.framework42.creditcheck.exceptions.CreditCheckException;
+import uc_webservice.Group;
+import uc_webservice.Message;
+import uc_webservice.UcReply;
+
+import java.util.List;
+
+public enum BaseParser {
+
+    INSTANCE;
+
+    private final Logger logger = Logger.getLogger("org.nummer42.creditcheck");
+
+    public Group findResponseGroup(UcReply reply, String groupId, int reportLevel) {
+
+        List<Group> groups = reply.getUcReport().get(reportLevel).getXmlReply().getReports().get(0).getReport().get(0).getGroup();
+
+        for(Group g: groups) {
+
+            if(groupId.equals(g.getId())) {
+                return g;
+            }
+        }
+
+        throw new IllegalArgumentException("No decision group found in uc reply (id: "+groupId+")");
+    }
+
+    public void validateReplyAndStatus(UcReply reply, String governmentId) throws CreditCheckException {
+
+        if(reply == null) {
+            logger.info("Illegal government id of value "+governmentId+" in credit check");
+            throw new CreditCheckException(999, "Illegal government id of value "+governmentId);
+        }
+
+        if("error".equals(reply.getStatus().getResult())) {
+
+            Message errorMess = reply.getStatus().getMessage();
+
+            logger.info(Integer.parseInt(errorMess.getId())+": "+ errorMess.getValue());
+            throw new CreditCheckException(Integer.parseInt(errorMess.getId()), errorMess.getValue());
+        }
+    }
+
+}
