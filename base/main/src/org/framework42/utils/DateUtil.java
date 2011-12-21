7d04f42799aa126d1442a1819d2da67e6cc91e47
drbizzaro <drbizzaro@97581d6c-679a-47f2-af3c-956745aba767>
2011-12-21 16:03:30 +0000

    * Many refactorings. * My Pages.

--- base/main/src/org/framework42/utils/DateUtil.java
+++ base/main/src/org/framework42/utils/DateUtil.java
@@ -100,4 +100,29 @@ public class DateUtil {
         return cal.getTime();
     }
 
+    public static Date getFromGovernmentId(String governmentId) {
+
+        if(governmentId==null || governmentId.length()<1) {
+
+            return null;
+        }
+
+        int year = Integer.parseInt(governmentId.substring(0, 2));
+        int month = Integer.parseInt(governmentId.substring(2, 4));
+        int day = Integer.parseInt(governmentId.substring(4, 6));
+
+        if( (2000+year) <= getInstance().get(YEAR)) {
+
+            year += 2000;
+
+        } else {
+
+            year += 1900;
+        }
+
+        Calendar cal = new GregorianCalendar(year, month-1, day);
+
+        return cal.getTime();
+    }
+
 }
