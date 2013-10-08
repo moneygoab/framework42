e0ce8bfe3516bf1319a2b7b6831d42fdd71de686
drbizzaro <drbizzaro@97581d6c-679a-47f2-af3c-956745aba767>
2013-10-08 15:00:47 +0000

    * Misc updates

--- framework42.financial/main/src/org/framework42/services/AnnuityCalculator.java
+++ framework42.financial/main/src/org/framework42/services/AnnuityCalculator.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 public interface AnnuityCalculator {
 
-    public int getMinimumToPay(BigDecimal initialAmount, BigDecimal monthlyInterest, int monthsToPayBack);
+    public int getMinimumToPay(BigDecimal initialAmount, BigDecimal interest, int monthsToPayBack);
 
     public double getMinimumToPayNotRounded(BigDecimal initialAmount, BigDecimal interest, int monthsToPayBack);
 
