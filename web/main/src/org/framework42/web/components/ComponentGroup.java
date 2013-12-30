3f22122accef95ff9b3caa97524e36b4d82add11
drbizzaro <drbizzaro@97581d6c-679a-47f2-af3c-956745aba767>
2013-12-30 06:40:40 +0000

    * Responsive design

--- web/main/src/org/framework42/web/components/ComponentGroup.java
+++ web/main/src/org/framework42/web/components/ComponentGroup.java
@@ -7,42 +7,50 @@ import java.util.List;
 
 public class ComponentGroup extends HtmlComponent {
 
-   private final List<HtmlComponent> components;
+    private final List<HtmlComponent> components;
 
-   private ComponentGroup(Builder builder) {
+    private ComponentGroup(Builder builder) {
 
-      this.components = builder.components;
+        this.components = builder.components;
 
-      this.tabOffset = 0;
+        this.tabOffset = 0;
 
-   }
+    }
 
-   @Override
-   protected void generateHtmlSpecific(WebPage page, HtmlComponent parent, boolean onSameRow) {
+    @Override
+    protected void generateHtmlSpecific(WebPage page, HtmlComponent parent, boolean onSameRow) {
 
-      for(HtmlComponent comp: components){
-         htmlBuilder.append(comp.getHtml(page, this, false));
-      }
+        for(HtmlComponent comp: components){
+            htmlBuilder.append(comp.getHtml(page, this, false));
+        }
 
-   }
+    }
 
-   public static class Builder implements ComponentBuilder<ComponentGroup>, HtmlComponentStorage<HtmlComponent>{
+    public static class Builder implements ComponentBuilder<ComponentGroup>, HtmlComponentStorage<HtmlComponent>{
 
-      private final List<HtmlComponent> components;
+        private final List<HtmlComponent> components;
 
-      public Builder() {
-         this.components = new ArrayList<HtmlComponent>();
-      }
+        public Builder() {
+            this.components = new ArrayList<HtmlComponent>();
+        }
 
-      @Override
-      public void add(HtmlComponent component) {
-         components.add(component);
-      }
+        public void add(HtmlComponent... htmlComponent) {
 
-      @Override
-      public ComponentGroup build() {
-         return new ComponentGroup(this);
-      }
-   }
+            for(HtmlComponent comp: htmlComponent) {
+
+                components.add(comp);
+            }
+        }
+
+        @Override
+        public void add(HtmlComponent component) {
+            components.add(component);
+        }
+
+        @Override
+        public ComponentGroup build() {
+            return new ComponentGroup(this);
+        }
+    }
 
 }
---
+++ web/main/src/org/framework42/web/components/FontSize.java
@@ -0,0 +1,7 @@
+package org.framework42.web.components;
+
+public enum FontSize {
+
+    NORMAL, SMALL, LARGE;
+
+}
