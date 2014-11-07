c30814ddcaada4d2195a19bbc89f4497a3db34ee
fredrikg <fredrikg@97581d6c-679a-47f2-af3c-956745aba767>
2014-11-07 16:02:33 +0000

    * Partner Payment Service

--- web/main/src/org/framework42/web/pages/RESTPoint.java
+++ web/main/src/org/framework42/web/pages/RESTPoint.java
@@ -20,43 +20,54 @@ import static org.framework42.web.pagemodel.RESTErrorCode.*;
 
 public abstract class RESTPoint extends HttpServlet {
 
-        protected final static Logger logger = Logger.getLogger("org.framework42.web");
+    protected final static Logger logger = Logger.getLogger("org.framework42.web");
 
-        protected int consumerId;
+    protected int consumerId;
 
-        protected String consumerKeyParameterName = "X-Consumer-Key";
+    protected String consumerKeyParameterName = "X-Consumer-Key";
 
-        protected APIResponseType defaultResponseType = APIResponseType.JSON;
+    protected APIResponseType defaultResponseType = APIResponseType.JSON;
 
-        protected boolean forceDataType = false;
+    protected boolean forceDataType = false;
 
-        protected boolean forceContentTypeInput = false;
+    protected boolean forceContentTypeInput = false;
 
-        protected boolean test = true;
+    protected boolean test = true;
 
-        protected List<APIResponseType> validResponseTypesList = Arrays.asList(APIResponseType.JSON);
+    protected List<APIResponseType> validResponseTypesList = Arrays.asList(APIResponseType.JSON);
 
-        protected List<String> validPostContentTypes = Arrays.asList("application/x-www-form-urlencoded");
+    protected List<String> validPostContentTypes = Arrays.asList("application/x-www-form-urlencoded");
 
-        protected List<String> validPutContentTypes = Arrays.asList("application/x-www-form-urlencoded");
+    protected List<String> validPutContentTypes = Arrays.asList("application/x-www-form-urlencoded");
 
-        @Override
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
 
             APIResponseType responseType = getResponseType(req.getHeader("Content-Type"), resp);
 
             consumerId = processCall(req, resp, responseType);
 
-            if(consumerId>0) {
+            if (consumerId > 0) {
 
                 doGetSpecific(req, resp, responseType, consumerId);
             }
+
+        } catch(Exception e) {
+
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getOutputStream().println("Unhandled internal error, can't give proper error feedback.");
+            logger.fatal(e.getMessage());
         }
+    }
 
-        protected abstract void doGetSpecific(HttpServletRequest req, HttpServletResponse resp, APIResponseType responseType, int consumerId);
+    protected abstract void doGetSpecific(HttpServletRequest req, HttpServletResponse resp, APIResponseType responseType, int consumerId);
 
-        @Override
-        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
 
             APIResponseType responseType = getResponseType(req.getHeader("Content-Type"), resp);
 
@@ -69,181 +80,206 @@ public abstract class RESTPoint extends HttpServlet {
                     doPostSpecific(req, resp, responseType, consumerId);
                 }
             }
+
+        } catch(Exception e) {
+
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getOutputStream().println("Unhandled internal error, can't give proper error feedback.");
+            logger.fatal(e.getMessage());
         }
+    }
 
-        protected abstract void doPostSpecific(HttpServletRequest req, HttpServletResponse resp, APIResponseType responseType, int consumerId);
+    protected abstract void doPostSpecific(HttpServletRequest req, HttpServletResponse resp, APIResponseType responseType, int consumerId);
 
-        @Override
-        protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
 
             APIResponseType responseType = getResponseType(req.getHeader("Content-Type"), resp);
 
             consumerId = processCall(req, resp, responseType);
 
-            if(consumerId>0) {
+            if (consumerId > 0) {
 
                 doPutSpecific(req, resp, responseType, consumerId);
             }
+
+        } catch(Exception e) {
+
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getOutputStream().println("Unhandled internal error, can't give proper error feedback.");
+            logger.fatal(e.getMessage());
         }
+    }
 
-        protected abstract void doPutSpecific(HttpServletRequest req, HttpServletResponse resp, APIResponseType responseType, int consumerId);
+    protected abstract void doPutSpecific(HttpServletRequest req, HttpServletResponse resp, APIResponseType responseType, int consumerId);
 
-        @Override
-        protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
 
             APIResponseType responseType = getResponseType(req.getHeader("Content-Type"), resp);
 
             consumerId = processCall(req, resp, responseType);
 
-            if(consumerId>0) {
+            if (consumerId > 0) {
 
                 doDeleteSpecific(req, resp, responseType, consumerId);
             }
+
+        } catch(Exception e) {
+
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getOutputStream().println("Unhandled internal error, can't give proper error feedback.");
+            logger.fatal(e.getMessage());
         }
+    }
 
-        protected abstract void doDeleteSpecific(HttpServletRequest req, HttpServletResponse resp, APIResponseType responseType, int consumerId);
+    protected abstract void doDeleteSpecific(HttpServletRequest req, HttpServletResponse resp, APIResponseType responseType, int consumerId);
 
-        protected int processCall(HttpServletRequest req, HttpServletResponse resp, APIResponseType responseType) {
+    protected int processCall(HttpServletRequest req, HttpServletResponse resp, APIResponseType responseType) {
 
-            consumerId = 0;
+        consumerId = 0;
 
-            if(req.getParameter(consumerKeyParameterName)!=null && req.getHeader(consumerKeyParameterName)==null) {
+        if(req.getParameter(consumerKeyParameterName)!=null && req.getHeader(consumerKeyParameterName)==null) {
 
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                addError(resp, INVALID_CONSUMER_KEY_PARAMETER_TYPE, responseType);
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            addError(resp, INVALID_CONSUMER_KEY_PARAMETER_TYPE, responseType);
 
-            } else if(req.getHeader(consumerKeyParameterName)==null) {
+        } else if(req.getHeader(consumerKeyParameterName)==null) {
 
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                addError(resp, MISSING_CONSUMER_KEY, responseType);
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            addError(resp, MISSING_CONSUMER_KEY, responseType);
 
-            } else {
+        } else {
 
-                String cid = req.getHeader(consumerKeyParameterName);
+            String cid = req.getHeader(consumerKeyParameterName);
 
-                if(cid==null) {
+            if(cid==null) {
 
-                    cid = req.getParameter(consumerKeyParameterName);
-                }
-
-                consumerId = getConsumerId(test, cid, APIRequestType.GET);
-
-                if(consumerId==0) {
-
-                    resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
-                    addError(resp, INVALID_CONSUMER_KEY, responseType);
-                }
+                cid = req.getParameter(consumerKeyParameterName);
             }
 
-            return consumerId;
+            consumerId = getConsumerId(test, cid, APIRequestType.GET);
+
+            if(consumerId==0) {
+
+                resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                addError(resp, INVALID_CONSUMER_KEY, responseType);
+            }
         }
 
-        protected abstract int getConsumerId(boolean test, String consumerKey, APIRequestType requestType);
+        return consumerId;
+    }
 
-        protected APIResponseType getResponseType(String responseParameter, HttpServletResponse resp) {
+    protected abstract int getConsumerId(boolean test, String consumerKey, APIRequestType requestType);
 
-            APIResponseType responseType = APIResponseType.NONE;
-            try {
+    protected APIResponseType getResponseType(String responseParameter, HttpServletResponse resp) {
 
-                if(responseParameter!=null) {
+        APIResponseType responseType = APIResponseType.NONE;
+        try {
 
-                    responseType = APIResponseType.getByName(responseParameter, forceDataType);
-                }
+            if(responseParameter!=null) {
 
-                if(responseType==APIResponseType.NONE && forceDataType) {
+                responseType = APIResponseType.getByName(responseParameter, forceDataType);
+            }
 
-                    resp.setStatus(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
-                    addError(resp, RESTErrorCode.INVALID_CONTENT_TYPE_FORCED, defaultResponseType);
-
-                } else if(responseType==APIResponseType.NONE) {
-
-                    responseType = defaultResponseType;
-                }
-
-                boolean notValidResponseType = true;
-                for(APIResponseType valid: validResponseTypesList) {
-
-                    if(valid==responseType) {
-
-                        notValidResponseType = false;
-                    }
-                }
-                if(notValidResponseType) {
-
-                    String validString = "";
-                    for(APIResponseType v: validResponseTypesList) {
-                        validString += v.getMimeType()+",";
-                    }
-                    validString = validString.substring(0, validString.length()-1);
-
-                    resp.setStatus(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
-                    addError(resp, "41501", "Content-Type with value "+responseParameter+" isn't supported. Please use one of the valid types ["+validString+"].", "General technical problem.", defaultResponseType);
-                }
-
-            } catch(Exception e) {
+            if(responseType==APIResponseType.NONE && forceDataType) {
 
                 resp.setStatus(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
-                addError(resp, RESTErrorCode.INVALID_CONTENT_TYPE, defaultResponseType);
-                logger.debug("Requested response type "+responseParameter+" don't exist!");
+                addError(resp, RESTErrorCode.INVALID_CONTENT_TYPE_FORCED, defaultResponseType);
+
+            } else if(responseType==APIResponseType.NONE) {
+
+                responseType = defaultResponseType;
             }
 
-            return responseType;
+            boolean notValidResponseType = true;
+            for(APIResponseType valid: validResponseTypesList) {
+
+                if(valid==responseType) {
+
+                    notValidResponseType = false;
+                }
+            }
+            if(notValidResponseType) {
+
+                String validString = "";
+                for(APIResponseType v: validResponseTypesList) {
+                    validString += v.getMimeType()+",";
+                }
+                validString = validString.substring(0, validString.length()-1);
+
+                resp.setStatus(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+                addError(resp, "41501", "Content-Type with value "+responseParameter+" isn't supported. Please use one of the valid types ["+validString+"].", "General technical problem.", defaultResponseType);
+            }
+
+        } catch(Exception e) {
+
+            resp.setStatus(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+            addError(resp, RESTErrorCode.INVALID_CONTENT_TYPE, defaultResponseType);
+            logger.debug("Requested response type "+responseParameter+" don't exist!");
         }
 
-        protected  boolean validateContentType(HttpServletResponse resp, APIResponseType responseType, List<String> validList, String contentType) {
+        return responseType;
+    }
 
-            if(!forceContentTypeInput) {
+    protected  boolean validateContentType(HttpServletResponse resp, APIResponseType responseType, List<String> validList, String contentType) {
+
+        if(!forceContentTypeInput) {
+
+            return true;
+        }
+
+        for(String validPost: validList) {
+
+            if(validPost.equalsIgnoreCase(contentType)) {
 
                 return true;
             }
-
-            for(String validPost: validList) {
-
-                if(validPost.equalsIgnoreCase(contentType)) {
-
-                    return true;
-                }
-            }
-
-            String validTypes = "";
-            for(String validPost: validList) {
-                validTypes += validPost+", ";
-            }
-            validTypes = validTypes.substring(0, validTypes.length()-2);
-
-            addError(resp, "41501", "Content-Type with value "+contentType+" isn't supported. Please use one of the valid types ("+validTypes+").", "General technical problem.", responseType);
-
-            return false;
         }
 
-        protected void addError(HttpServletResponse resp, RESTErrorCode errorCode, APIResponseType responseType) {
-
-            addError(resp, errorCode.getId(), errorCode.getErrorMessage(), errorCode.getEndUserErrorMessage(), responseType);
+        String validTypes = "";
+        for(String validPost: validList) {
+            validTypes += validPost+", ";
         }
+        validTypes = validTypes.substring(0, validTypes.length()-2);
 
-        protected void addError(HttpServletResponse resp, String errorCode, String errorMessage, String endUserErrorMessage, APIResponseType responseType) {
+        addError(resp, "41501", "Content-Type with value "+contentType+" isn't supported. Please use one of the valid types ("+validTypes+").", "General technical problem.", responseType);
 
-            try {
+        return false;
+    }
 
-                if(responseType==APIResponseType.JSON) {
+    protected void addError(HttpServletResponse resp, RESTErrorCode errorCode, APIResponseType responseType) {
 
-                    Map<String,String> errorMap = new HashMap<String, String>();
+        addError(resp, errorCode.getId(), errorCode.getErrorMessage(), errorCode.getEndUserErrorMessage(), responseType);
+    }
 
-                    errorMap.put("error_code", errorCode);
-                    errorMap.put("error_message", errorMessage);
-                    errorMap.put("end_user_error_message", endUserErrorMessage);
+    protected void addError(HttpServletResponse resp, String errorCode, String errorMessage, String endUserErrorMessage, APIResponseType responseType) {
 
-                    JSONObject errorObj = new JSONObject(errorMap);
+        try {
 
-                    resp.getWriter().println(errorObj.toString(2));
+            if(responseType==APIResponseType.JSON) {
 
-                }
+                Map<String,String> errorMap = new HashMap<String, String>();
 
-            } catch(IOException e) {
+                errorMap.put("error_code", errorCode);
+                errorMap.put("error_message", errorMessage);
+                errorMap.put("end_user_error_message", endUserErrorMessage);
 
-                logger.fatal(e.getMessage());
+                JSONObject errorObj = new JSONObject(errorMap);
+
+                resp.getWriter().println(errorObj.toString(2));
+
             }
+
+        } catch(IOException e) {
+
+            logger.fatal(e.getMessage());
         }
+    }
 
 
 }
