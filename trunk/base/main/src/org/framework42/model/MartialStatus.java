17af48fb08d4447a303aa3a88aa940532e5d1e23
fredrikg <fredrikg@97581d6c-679a-47f2-af3c-956745aba767>
2015-01-05 16:13:14 +0000

    * Partner Payment Service

---
+++ trunk/base/main/src/org/framework42/model/MartialStatus.java
@@ -0,0 +1,30 @@
+package org.framework42.model;
+
+public enum MartialStatus {
+
+    SINGLE(1), MARRIED(2), DIVORCED(3), WIDOWED(4), UNKNOWN(0);
+
+    private final int id;
+
+    MartialStatus(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static MartialStatus getById(int id) {
+
+        for(MartialStatus martialStatus: MartialStatus.values()) {
+
+            if(martialStatus.getId() == id) {
+
+                return martialStatus;
+            }
+        }
+
+        throw new IllegalArgumentException("No martial status with id "+id+" exists!");
+    }
+
+}
