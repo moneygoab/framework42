a4795c073072f89da563a003e08e86c2f1540950
fredrikg <fredrikg@97581d6c-679a-47f2-af3c-956745aba767>
2015-02-13 16:01:09 +0000

    * Partner Service

--- base/main/src/org/framework42/utils/services/impl/GoogleMailSender.java
+++ base/main/src/org/framework42/utils/services/impl/GoogleMailSender.java
@@ -48,4 +48,42 @@ public enum GoogleMailSender implements MailSender {
 
     }
 
+    @Override
+    public void sendHtmlMail(String userName, String password, String toAddress, String subject, String htmlMessage, String fallbackMessage) {
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        Session mailSession = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+
+                        return new PasswordAuthentication("info@moneygo.se", "kvittraommoney");
+                    }
+                });
+
+        try {
+
+            Message messageObject = new MimeMessage(mailSession);
+            messageObject.setFrom(new InternetAddress("Moneygo kundtjänst <info@moneygo.se>"));
+            messageObject.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(toAddress));
+            messageObject.setSubject(subject);
+            messageObject.setText(fallbackMessage);
+
+            messageObject.setContent(htmlMessage, "text/html");
+
+            Transport.send(messageObject);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
