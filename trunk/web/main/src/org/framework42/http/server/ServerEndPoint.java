a3f3451295c555c269621fec2529d747fe2a05b3
fredrikg <fredrikg@97581d6c-679a-47f2-af3c-956745aba767>
2015-07-24 15:03:10 +0000

    * Partner Service

--- trunk/web/main/src/org/framework42/http/server/ServerEndPoint.java
+++ trunk/web/main/src/org/framework42/http/server/ServerEndPoint.java
@@ -1,11 +1,16 @@
 package org.framework42.http.server;
 
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import org.framework42.utils.DateUtil;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
-import java.util.Map;
+
+import static org.framework42.http.StatusCode.*;
 
 public abstract class ServerEndPoint<R extends RequestData> {
+
+    private final static SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMMM YYYY HH:mm:ss");
 
     private final List<String> path;
 
@@ -38,5 +43,57 @@ public abstract class ServerEndPoint<R extends RequestData> {
     }
 
     public abstract String renderEndPointResponse(R req, RequestData data);
+
+    protected String renderGeneralNotImplementedResponse(HttpServerEnvironment serverEnvironment) {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("HTTP/1.1 "+ NOT_IMPLEMENTED_501.getId()+" "+NOT_IMPLEMENTED_501.getName()+"\r\n");
+        sb.append("Date: "+dateFormat.format(DateUtil.stepBack(new Date(), 3600000))+" GMT\r\n");
+        sb.append("Server: "+serverEnvironment.getServerName()+"\r\n");
+        sb.append("Connection: close\r\n");
+
+        return sb.toString();
+    }
+
+    protected String renderEndPointGetResponse(HttpServerEnvironment serverEnvironment) {
+
+        return renderGeneralNotImplementedResponse(serverEnvironment);
+    }
+
+    private String renderEndPointHeadResponse(HttpServerEnvironment serverEnvironment) {
+
+        return renderGeneralNotImplementedResponse(serverEnvironment);
+    }
+
+    private String processPost(HttpServerEnvironment serverEnvironment) {
+
+        return renderGeneralNotImplementedResponse(serverEnvironment);
+    }
+
+    private String processPut(HttpServerEnvironment serverEnvironment) {
+
+        return renderGeneralNotImplementedResponse(serverEnvironment);
+    }
+
+    private String processDelete(HttpServerEnvironment serverEnvironment) {
+
+        return renderGeneralNotImplementedResponse(serverEnvironment);
+    }
+
+    private String processConnect(HttpServerEnvironment serverEnvironment) {
+
+        return renderGeneralNotImplementedResponse(serverEnvironment);
+    }
+
+    private String processOptions(HttpServerEnvironment serverEnvironment) {
+
+        return renderGeneralNotImplementedResponse(serverEnvironment);
+    }
+
+    private String processTrace(HttpServerEnvironment serverEnvironment) {
+
+        return renderGeneralNotImplementedResponse(serverEnvironment);
+    }
 
 }
