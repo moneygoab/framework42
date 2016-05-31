ccd6d42c2d3e7432bf6bde99dcc74a8c396b2143
Fredrik Gustavsson <fredrik.gustavsson@moneygo.se>
2016-05-31 11:44:29 +0200

    Added rest input parser for special empty cases

--- web/main/src/org/framework42/web/utils/rest/RESTInputParser.java
+++ web/main/src/org/framework42/web/utils/rest/RESTInputParser.java
@@ -73,6 +73,57 @@ public enum RESTInputParser {
         }
     }
 
+    public JSONObject parseInDataAllowEmpty(HttpServletRequest req, HttpServletResponse resp, APIResponseType responseType) throws IOException, StopServletExecutionException {
+
+        StringBuilder buffer = new StringBuilder();
+
+        try {
+
+            req.setCharacterEncoding("UTF-8");
+            BufferedReader reader = req.getReader();
+
+            String inLine;
+            while ((inLine = reader.readLine()) != null) {
+                buffer.append(inLine);
+            }
+            String data = buffer.toString();
+
+            logger.debug("Input data: "+data);
+
+            if(data.length()==0 || !data.startsWith("{")) {
+
+                return new JSONObject();
+            }
+
+            return new JSONObject(data);
+
+        } catch(UnsupportedEncodingException e) {
+
+            logger.error(e.getClass().toString() + ": " + e.getMessage());
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, RESTErrorMaker.INSTANCE.addError("40003", "Bad encoding of input data it should be in UTF-8!", "General technical problem.", responseType));
+            throw new StopServletExecutionException();
+
+        } catch(MalformedInputException e) {
+
+            logger.fatal(e.getClass().toString() + ": " + e.getMessage());
+            logger.fatal("Data:" + buffer.toString());
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, RESTErrorMaker.INSTANCE.addError("40003", "Malformed input, can't parse input data.", "General technical problem.", responseType));
+            throw new StopServletExecutionException();
+
+        } catch(IOException e) {
+
+            logger.error(e.getClass().toString() + ": " + e.getMessage());
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, RESTErrorMaker.INSTANCE.addError("50009", "Internal server error, couldn't complete call.", "General technical problem.", responseType));
+            throw new StopServletExecutionException();
+
+        } catch(JSONException e) {
+
+            logger.error(e.getClass().toString() + ": " + e.getMessage());
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, RESTErrorMaker.INSTANCE.addError("40027", "Badly formatted input data. Not valid of chosen type " + responseType.getMimeType(), "General technical problem.", responseType));
+            throw new StopServletExecutionException();
+        }
+    }
+
     public JSONArray parseInDataArray(HttpServletRequest req, HttpServletResponse resp, APIResponseType responseType) throws StopServletExecutionException, IOException {
 
         try {
@@ -94,6 +145,49 @@ public enum RESTInputParser {
                 logger.error("A JSONArray data object must begin with '[' at 1 [character 2 line 1]");
                 resp.sendError(HttpServletResponse.SC_BAD_REQUEST, RESTErrorMaker.INSTANCE.addError("40027", "Badly formatted input data. Not valid of chosen type "+responseType.getMimeType(), "General technical problem.", responseType));
                 throw new StopServletExecutionException();
+            }
+
+            return new JSONArray(data);
+
+        } catch(UnsupportedEncodingException e) {
+
+            logger.error(e.getClass().toString() + ": " + e.getMessage());
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, RESTErrorMaker.INSTANCE.addError("40003", "Bad encoding of input data it should be in UTF-8!", "General technical problem.", responseType));
+            throw new StopServletExecutionException();
+
+        } catch(IOException e) {
+
+            logger.error(e.getClass().toString() + ": " + e.getMessage());
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, RESTErrorMaker.INSTANCE.addError("50001", "Internal communication error, application can't be completed!", "General technical problem.", responseType));
+            throw new StopServletExecutionException();
+
+        } catch(JSONException e) {
+
+            logger.error(e.getClass().toString() + ": " + e.getMessage());
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, RESTErrorMaker.INSTANCE.addError("40027", "Badly formatted input data. Not valid of chosen type "+responseType.getMimeType(), "General technical problem.", responseType));
+            throw new StopServletExecutionException();
+        }
+    }
+
+    public JSONArray parseInDataArrayAllowEmpty(HttpServletRequest req, HttpServletResponse resp, APIResponseType responseType) throws StopServletExecutionException, IOException {
+
+        try {
+
+            StringBuilder buffer = new StringBuilder();
+            req.setCharacterEncoding("UTF-8");
+            BufferedReader reader = req.getReader();
+
+            String inLine;
+            while ((inLine = reader.readLine()) != null) {
+                buffer.append(inLine);
+            }
+            String data = buffer.toString();
+
+            logger.debug(data);
+
+            if(data.length()==0 || !data.startsWith("[")) {
+
+                return new JSONArray();
             }
 
             return new JSONArray(data);
