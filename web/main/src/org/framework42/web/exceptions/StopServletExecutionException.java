1ede39dfd80cfe79573deb0ac0d661fc9363dbf3
Marcus Ling <marcus.ling@moneygo.se>
2017-02-27 16:23:25 +0100

    added message for StopServletExecutionException

--- web/main/src/org/framework42/web/exceptions/StopServletExecutionException.java
+++ web/main/src/org/framework42/web/exceptions/StopServletExecutionException.java
@@ -2,6 +2,12 @@ package org.framework42.web.exceptions;
 
 public class StopServletExecutionException extends Exception {
 
-    
+    public StopServletExecutionException(){
+        super();
+    }
+
+    public StopServletExecutionException(String message){
+        super(message);
+    }
 
 }
