3c2c6ff3037042c8b493f8f537822e849becbfdc
Marcus Ling <marcus.ling@moneygo.se>
2017-11-07 10:20:47 +0100

    added ifs before or after Executor

--- base/main/src/org/framework42/annotationsinterface/ExecutorListener.java
+++ base/main/src/org/framework42/annotationsinterface/ExecutorListener.java
@@ -1,11 +1,13 @@
 package org.framework42.annotationsinterface;
 
 
+import org.framework42.model.service.ExecuteRunType;
+
 /**
  * Created by ling on 2016-12-15.
  */
 public interface ExecutorListener {
 
-    void invokeMethod(int id, ExecutorObject object);
+    void invokeMethod(int id, ExecutorObject object, ExecuteRunType type);
 
 }
