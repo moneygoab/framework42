22835961b295526f572aa86304816a81b421f1bd
Fredrik Gustavsson <fredrik.gustavsson@moneygo.se>
2018-03-23 16:41:58 +0100

    Added option for loading config from server in ApplicationConfigHandler

--- base/main/src/org/framework42/configuration/ApplicationConfigHandler.java
+++ base/main/src/org/framework42/configuration/ApplicationConfigHandler.java
@@ -1,10 +1,14 @@
 package org.framework42.configuration;
 
 import org.apache.log4j.Logger;
+import org.json.RESTJSONCaller;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
@@ -46,6 +50,72 @@ public enum ApplicationConfigHandler {
                 throw new RuntimeException(errorMess+"\n"+e+"\n"+ex);
 
             }
+        }
+
+        if("true".equalsIgnoreCase(properties.getProperty("load_config_from_server"))) {
+
+            try {
+
+                properties = load(properties.getProperty("settings_server_url"), properties.getProperty("setting_consumer_key_parameter_name"), properties.getProperty("setting_consumer_key"));
+
+            } catch (Exception e) {}
+        }
+
+        return properties;
+
+    }
+
+    public static void main(String[] args) {
+
+        ApplicationConfigHandler.INSTANCE.load("https://settings.moneygo.se/v1/settings", "X-Moneygo-Consumer-Key", "guss2");
+    }
+
+    public Properties load(String settingsServerUrl, String consumerKeyParameterName, String consumerKey) {
+
+        Properties properties = new Properties();
+
+        try {
+
+            URL url;
+            HttpURLConnection connection = null;
+            try {
+                url = new URL(settingsServerUrl);
+
+                connection = (HttpURLConnection)url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty(consumerKeyParameterName, consumerKey);
+                connection.setRequestProperty("Content-Type", "application/xml");
+
+                connection.setUseCaches (false);
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+
+                //Get Response
+                if(connection.getResponseCode()==HttpURLConnection.HTTP_OK || connection.getResponseCode()==HttpURLConnection.HTTP_CREATED) {
+
+                    properties.loadFromXML(connection.getInputStream());
+
+                } else {
+
+                    String errorMess = "Problem loading settings file from server "+settingsServerUrl+"Got HTTP response "+connection.getResponseCode();
+
+                    logger.fatal(errorMess);
+                    throw new RuntimeException(errorMess);
+                }
+
+            } finally {
+
+                if(connection != null) {
+                    connection.disconnect();
+                }
+            }
+
+        } catch(Exception ex) {
+
+                String errorMess = "Problem loading settings file from server "+settingsServerUrl;
+
+                logger.fatal(errorMess+"\n"+ex+"\n"+ex);
+                throw new RuntimeException(errorMess+"\n"+ex+"\n"+ex);
         }
 
         return properties;
