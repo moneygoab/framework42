9b64514524bafe0190c8744cc7bfb1415e1b1d44
Marcus Ling <marcus.ling@moneygo.se>
2018-05-28 15:44:38 +0200

    added esigning defualt for channel

--- framework42.creditcheck/main/src/org/framework42/creditcheck/model/ApplicationChannel.java
+++ framework42.creditcheck/main/src/org/framework42/creditcheck/model/ApplicationChannel.java
@@ -5,13 +5,25 @@ import java.time.LocalDateTime;
 public enum ApplicationChannel {
 
     NONE(0), INTERNET(1), PHONE(2), LETTER(3), DIRECT_MARKETING(4), TELEPHONE_MARKETING(5),
-    COMPRICER(6), CREDIT_FINDER(11), FREEDOM_FINANCE(12), LENDO(15), MYLOAN(14), ADVISA(13), CONSECTOR(16), SAMBLA(17), ZENSUM(19),
+    COMPRICER(6), CREDIT_FINDER(11,true), FREEDOM_FINANCE(12,true), LENDO(15,true), MYLOAN(14,true), ADVISA(13,true), CONSECTOR(16,true), SAMBLA(17,true), ZENSUM(19,true),
     EMAIL(7), AFFILIATE_EUROADS(8), AFFILIATE_ADTRACTION(9), AFFILIATE_GOOGLE_ADWORDS(10), AFFILIATE_EMERCE(18), PARTNER(50);
 
     private final int id;
+    private final boolean esigning;
 
     ApplicationChannel(int id) {
         this.id = id;
+        esigning = false;
+    }
+
+    ApplicationChannel(int id, boolean esigning) {
+        this.id = id;
+        this.esigning = esigning;
+    }
+
+
+    public boolean isEsigning() {
+        return esigning;
     }
 
     public int getId() {
