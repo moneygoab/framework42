01fa352bded2bc2d95555bd90255df1acdd4e6dd
Fredrik Gustavsson <fredrik.gustavsson@moneygo.se>
2018-10-25 15:32:00 +0200

    Update in LocalDateTimeUtil

--- base/main/src/org/framework42/utils/LocalDateTimeUtil.java
+++ base/main/src/org/framework42/utils/LocalDateTimeUtil.java
@@ -2,7 +2,10 @@ package org.framework42.utils;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+
+import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
 
 public class LocalDateTimeUtil {
 
@@ -14,6 +17,26 @@ public class LocalDateTimeUtil {
     public static LocalDateTime getFromDate(Date date) {
 
         return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+    }
+
+    public static LocalDateTime toBeginningOfDay(LocalDateTime dateTime) {
+
+        return dateTime.withHour(0).withMinute(0).withSecond(0).withNano(0);
+    }
+
+    public static LocalDateTime toEndOfDay(LocalDateTime dateTime) {
+
+        return dateTime.withHour(23).withMinute(59).withSecond(59);
+    }
+
+    public static LocalDateTime toBeginningOfMonth(LocalDateTime dateTime) {
+
+        return dateTime.withDayOfMonth(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
+    }
+
+    public static LocalDateTime toEndOfMonth(LocalDateTime dateTime) {
+
+        return dateTime.withDayOfMonth(dateTime.toLocalDate().lengthOfMonth()).withHour(23).withMinute(59).withSecond(59);
     }
 
 }
