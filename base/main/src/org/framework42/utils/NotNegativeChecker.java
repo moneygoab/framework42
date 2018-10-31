09d9112f8ce2850a4584641fecbc66fd116d7b7a
Fredrik Gustavsson <fredrik.gustavsson@moneygo.se>
2018-10-31 13:51:26 +0100

    Expanded not negative checker with BigDecimal

--- base/main/src/org/framework42/utils/NotNegativeChecker.java
+++ base/main/src/org/framework42/utils/NotNegativeChecker.java
@@ -2,6 +2,8 @@ package org.framework42.utils;
 
 import org.apache.log4j.Logger;
 
+import java.math.BigDecimal;
+
 public class NotNegativeChecker {
 
     private final static Logger logger = Logger.getLogger("org.framework42");
@@ -107,6 +109,34 @@ public class NotNegativeChecker {
     public static double notNegative(double variable, String errorMessage, Logger logger) {
 
         if(variable < 0) {
+            logger.error(errorMessage);
+            throw new IllegalArgumentException(errorMessage);
+        }
+
+        return variable;
+    }
+
+
+    public static BigDecimal notNegative(BigDecimal variable) {
+
+        return notNegative(variable, "BigDecimal can't be negative", logger);
+    }
+
+    public static BigDecimal notNegative(BigDecimal variable, String errorMessage) {
+
+        return notNegative(variable, errorMessage, logger);
+    }
+
+    public static BigDecimal notNegative(BigDecimal variable, String errorMessage, String loggerId) {
+
+        Logger logger = Logger.getLogger(loggerId);
+
+        return notNegative(variable, errorMessage, logger);
+    }
+
+    public static BigDecimal notNegative(BigDecimal variable, String errorMessage, Logger logger) {
+
+        if(variable.compareTo(BigDecimal.ZERO) < 0) {
             logger.error(errorMessage);
             throw new IllegalArgumentException(errorMessage);
         }
