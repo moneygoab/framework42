6a1c1c641d7af0600a100643d63db1e233ddfbc2
Marcus Ling <marcus.ling@moneygo.se>
2019-02-07 15:47:49 +0100

    added consumer key for restConsumer

--- web/main/src/org/framework42/web/authorization/RESTConsumer.java
+++ web/main/src/org/framework42/web/authorization/RESTConsumer.java
@@ -13,12 +13,15 @@ public class RESTConsumer {
 
     private final int internalId;
 
+    private final String consumerKey;
+
     private final Map<Role,UserRole> roleMap;
 
-    public RESTConsumer(boolean authenticated, int consumerId, int internalId, Map<Role, UserRole> roleMap) {
+    public RESTConsumer(boolean authenticated, int consumerId, int internalId,String consumerKey, Map<Role, UserRole> roleMap) {
         this.authenticated = authenticated;
         this.consumerId = consumerId;
         this.internalId = internalId;
+        this.consumerKey = consumerKey;
         this.roleMap = roleMap;
     }
 
@@ -32,6 +35,10 @@ public class RESTConsumer {
 
     public int getInternalId() {
         return internalId;
+    }
+
+    public String getConsumerKey() {
+        return consumerKey;
     }
 
     public Map<Role, UserRole> getRoleMap() {
