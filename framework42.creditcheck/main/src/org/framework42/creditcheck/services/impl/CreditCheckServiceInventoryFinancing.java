04482845ac87d2fad7d416b21c6e881bc96a6b63
Fredrik Gustavsson <fredrik.gustavsson@moneygo.se>
2021-02-26 11:11:07 +0100

    Fix Inventory financing credit bureue

--- framework42.creditcheck/main/src/org/framework42/creditcheck/services/impl/CreditCheckServiceInventoryFinancing.java
+++ framework42.creditcheck/main/src/org/framework42/creditcheck/services/impl/CreditCheckServiceInventoryFinancing.java
@@ -1,18 +1,12 @@
 package org.framework42.creditcheck.services.impl;
 
 import org.apache.log4j.Logger;
-import org.framework42.address.model.InformationProvider;
-import org.framework42.address.model.PostalCodeFormat;
-import org.framework42.address.model.impl.PostalCodeImpl;
-import org.framework42.address.model.impl.SimpleSecureAddressImpl;
 import org.framework42.creditcheck.exceptions.CreditCheckException;
 import org.framework42.creditcheck.model.*;
 import org.framework42.creditcheck.model.impl.ApplicantImpl;
-import org.framework42.creditcheck.model.impl.ApplicantNamesImpl;
 import org.framework42.creditcheck.model.impl.CreditBureauApplicationImpl;
 import org.framework42.creditcheck.model.impl.SimpleCreditBureauApplicationResponse;
 import org.framework42.creditcheck.services.CreditCheckService;
-import org.framework42.model.Country;
 import org.framework42.services.impl.MoneyImpl;
 import org.framework42.utils.LocalDateUtil;
 
@@ -60,21 +54,8 @@ public class CreditCheckServiceInventoryFinancing implements CreditCheckService 
                         application.getMainApplicant().getGovernmentId(),
                         BigDecimal.ZERO,
                         LocalDateUtil.getFromGovernmentId(application.getMainApplicant().getGovernmentId()),
-                        new ApplicantNamesImpl(
-                                "",
-                                "",
-                                ""
-                        ),
-                        new SimpleSecureAddressImpl(
-                                0,
-                                "",
-                                "",
-                                "",
-                                new PostalCodeImpl(PostalCodeFormat.getByCountry(Country.SWEDEN), ""),
-                                "",
-                                Country.SWEDEN,
-                                InformationProvider.POPULATION_REGISTERS
-                        ),
+                        application.getMainApplicant().getApplicantNames(),
+                        application.getMainApplicant().getAddress(),
                         application.getMainApplicant().getContactMethods(),
                         0
                 ),
